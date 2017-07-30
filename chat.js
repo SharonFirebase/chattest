2ffe4cb1c10235c512a59527f93ed4adf849d5be
SharonFirebase <30497899+SharonFirebase@users.noreply.github.com>
2017-07-30 15:29:15 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,3 +3,7 @@ var myFirebase = new Firebase('https://chattest-269df.firebaseio.com/');
 var usernameInput = document.querySelector('#username');
 var textInput = document.querySelector('#text');
 var postButton = document.querySelector('#post');
+
+postButton.addEventListener('click', function(){
+});
+
