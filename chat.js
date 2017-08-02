92fbbc591aa6b11dde8e4de55a8ebb80c7a3f789
SharonFirebase <30497899+SharonFirebase@users.noreply.github.com>
2017-08-02 09:42:10 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -21,6 +21,7 @@ var beginListening = function() {
       msgElement.appendChild(msgUsernameElement);
       msgElement.appendChild(msgTextElement);
       msgElement.className = "msg";
+       document.getElementById("results").style.textalign="center"; 
       document.getElementById("results").appendChild(msgElement); 
     });
    
